cc507343b538844dbceb8e01e5e804e775eed5b5
Vladimir Voblikov <vladimir.voblikov.774@gmail.com>
2023-04-05 00:54:13 +0500

    try add transactions.. (TIRED)

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,7 +2,6 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import java.util.List;
@@ -16,102 +15,66 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Session session = null;
-        Transaction transaction;
 
-        try {
-            session = Util.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-            session.createNativeQuery("""
+        String createUsersTable = """
                 CREATE TABLE IF NOT EXISTS users(
                 id INT PRIMARY KEY AUTO_INCREMENT,
                 name VARCHAR(60) NOT NULL,
                 lastName VARCHAR(60) NOT NULL,
                 age TINYINT UNSIGNED NOT NULL
-                );""", User.class).executeUpdate();
+                );""";
 
+        try (var session = Util.getSessionFactory().openSession()) {
+            var transaction = session.beginTransaction();
+            session.createNativeQuery(createUsersTable, User.class).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
-            // if (transaction != null) { transaction.rollback(); } rollback for DDL?
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (session != null) {
-                    session.close();
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
         }
-
     }
 
     @Override
     public void dropUsersTable() {
 
-        Session session = null;
-        Transaction transaction;
-
-        try {
-            session = Util.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-
-            session.createNativeQuery("""
+        String dropUsersTable = """
               DROP TABLE IF EXISTS users;
-             """, User.class).executeUpdate();
+             """;
 
+        try (var session = Util.getSessionFactory().openSession()) {
+            var transaction = session.beginTransaction();
+            session.createNativeQuery(dropUsersTable, User.class).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
-            // if (transaction != null) { transaction.rollback(); } rollback for DDL?
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (session != null) {
-                    session.close();
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
-        var result = String.format("User с именем – '%s' добавлен в базу данных", name);
-        Session session = null;
         Transaction transaction = null;
+        var result = String
+                .format("User с именем – '%s' добавлен в базу данных", name);
 
-        try {
-            session = Util.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-            session.persist(new User(name, lastName, age));
-            transaction.commit();
-            System.out.println(result);
+        try (var session = Util.getSessionFactory().openSession()) {
+                transaction = session.beginTransaction();
+                session.persist(new User(name, lastName, age));
+                transaction.commit();
+                System.out.println(result);
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (session != null) {
-                    session.close();
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 
     @Override
     public void removeUserById(long id) {
 
-        Session session = null;
         Transaction transaction = null;
 
-        try {
-            session = Util.getSessionFactory().openSession();
+        try (var session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             session.remove(session.find(User.class, id));
             transaction.commit();
@@ -120,53 +83,25 @@ public class UserDaoHibernateImpl implements UserDao {
                 transaction.rollback();
             }
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (session != null) {
-                    session.close();
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 
     @Override
     public List<User> getAllUsers() {
 
-        Session session = null;
-        Transaction transaction = null;
-
-        try {
-            session = Util.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-            var users = session.createQuery("from User", User.class).getResultList();
-            transaction.commit();
-            return users;
+        try (var session = Util.getSessionFactory().openSession()){
+            return session.createQuery("from User", User.class).getResultList();
         } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (session != null) {
-                    session.close();
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 
     @Override
     public void cleanUsersTable() {
 
-        Session session = null;
         Transaction transaction = null;
 
-        try {
-            session = Util.getSessionFactory().openSession();
+        try (var session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             var users = session.createQuery("from User", User.class).getResultList();
             for (var user: users) {
@@ -178,14 +113,6 @@ public class UserDaoHibernateImpl implements UserDao {
                 transaction.rollback();
             }
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (session != null) {
-                    session.close();
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 }
